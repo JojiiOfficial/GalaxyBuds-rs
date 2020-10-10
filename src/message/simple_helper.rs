8967b5f9142e08f490fe68e2fa64fc6e1b2924f2
jojii <jojii@gmx.net>
2020-10-11 00:33:18 +0200

    add set adjust sound sync

--- src/message/simple_helper.rs
+++ src/message/simple_helper.rs
@@ -6,3 +6,7 @@ use super::{
 pub fn new_equalizer(d: EqualizerType) -> simple::Simple {
     simple::new(ids::EQUALIZER, d.encode())
 }
+
+pub fn new_adjust_sound_sync(adjust: bool) -> simple::Simple {
+    simple::new(ids::ADJUST_SOUND_SYNC, adjust.into())
+}
